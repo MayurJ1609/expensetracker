b735d6827cc36868668749a6537d25c6298e06f3
Mayur <mayur.jaganmohan@gmail.com>
2020-05-09 22:19:24 +0530

    Updates

---
+++ precache-manifest.768cdd978b149cba9709dce6ec5734b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cc2d6de7051d7db0e21dbdeddcb0c2d",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "cb24a180f06607118ef6",
+    "url": "/expense-tracker/static/css/main.36cc5f7e.chunk.css"
+  },
+  {
+    "revision": "9d8ffab4baf95b81ca80",
+    "url": "/expense-tracker/static/js/2.3b6dfc78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.3b6dfc78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb24a180f06607118ef6",
+    "url": "/expense-tracker/static/js/main.bdd77518.chunk.js"
+  },
+  {
+    "revision": "e5ffa08fed2e84cd1726",
+    "url": "/expense-tracker/static/js/runtime-main.4b9ea18e.js"
+  }
+]);
