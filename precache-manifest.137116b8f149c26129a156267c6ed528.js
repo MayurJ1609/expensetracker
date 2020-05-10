93538dccd64cd488393a0d5482d1f80142e2bd57
Mayur <mayur.jaganmohan@gmail.com>
2020-05-10 12:54:47 +0530

    Updates

--- precache-manifest.137116b8f149c26129a156267c6ed528.js
+++ precache-manifest.137116b8f149c26129a156267c6ed528.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "254627ca7369c113760bf08a639df14a",
+    "revision": "c146de8c9150900ce83a941f5edbdaf6",
     "url": "/expensetracker/index.html"
   },
   {
-    "revision": "2e9fbcaf0b5c1a737140",
+    "revision": "0167cfbee95de45cf60f",
     "url": "/expensetracker/static/css/main.36cc5f7e.chunk.css"
   },
   {
-    "revision": "9d8ffab4baf95b81ca80",
-    "url": "/expensetracker/static/js/2.3b6dfc78.chunk.js"
+    "revision": "f35695db11317516d110",
+    "url": "/expensetracker/static/js/2.c3734ca7.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/expensetracker/static/js/2.3b6dfc78.chunk.js.LICENSE.txt"
+    "url": "/expensetracker/static/js/2.c3734ca7.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2e9fbcaf0b5c1a737140",
-    "url": "/expensetracker/static/js/main.4a46ae2a.chunk.js"
+    "revision": "0167cfbee95de45cf60f",
+    "url": "/expensetracker/static/js/main.d719fb6a.chunk.js"
   },
   {
     "revision": "764a5b9f6064c72d8ae9",
